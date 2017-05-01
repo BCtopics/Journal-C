3a320c26c12728521157213c80ed0f7a40a43dc0
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-05-01 15:27:01 -0600

    Master List View/Controller Implemented

---
+++ Journal-C/Journal-C/EntryListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  EntryListTableViewController.h
+//  Journal-C
+//
+//  Created by Bradley GIlmore on 5/1/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EntryListTableViewController : UITableViewController
+
+@end
---
+++ Journal-C/Journal-C/EntryListTableViewController.m
@@ -0,0 +1,85 @@
+//
+//  EntryListTableViewController.m
+//  Journal-C
+//
+//  Created by Bradley GIlmore on 5/1/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+#import "EntryListTableViewController.h"
+#import "EntryController.h"
+
+@interface EntryListTableViewController ()
+
+@end
+
+@implementation EntryListTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+#pragma mark - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [EntryController sharedInstance].entries.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"entryCell" forIndexPath:indexPath];
+    
+    EntryController *entryController = [EntryController sharedInstance];
+    Entry *entry = entryController.entries[indexPath.row];
+    
+    cell.textLabel.text = entry.title;
+    
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        Entry *entry = [EntryController sharedInstance].entries[indexPath.row];
+        [[EntryController sharedInstance] removeEntry:entry];
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+}
+
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
