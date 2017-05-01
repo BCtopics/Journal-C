1786c5b51fd3fdc9f2121f08a0363a32fa765373
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-05-01 14:27:39 -0600

    Project Cleanup

---
+++ Journal-C/Journal-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Journal-C
+//
+//  Created by Bradley GIlmore on 5/1/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
